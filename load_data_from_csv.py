d21fbee66e8f16dbd3a6ef44da10030abb905979
rochel05 <52916294+rochel05@users.noreply.github.com>
2019-07-15 17:28:24 +0200

    Add files via upload

---
+++ load_data_from_csv.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.utils import to_categorical
+
+def load_data_from_csv():
+    #test data
+    df_test = pd.read_csv('data/fashion_test.csv')
+    df_test_x = df_test.iloc[:, 0:784]
+    df_test_y = df_test['y']
+    first_image = df_test_x.loc[1, :]
+    first_label = df_test_y[1]
+    plt.imshow(np.reshape(first_image.values, (28, 28)), cmap='gray_r', interpolation='none')
+    plt.title('Digit Label: {}'.format(first_label))
+    plt.show()
+    sc = StandardScaler()
+    df_test_x = sc.fit_transform(df_test_x)
+    print('df_test rescaled : ', df_test_x)
+    print('df_test_y : ', df_test_y)
+
+    #train data
+    df_train = pd.read_csv('data/fashion_train.csv')
+    df_train_x = df_train.iloc[:, 0:784]
+    df_train_y = df_train['y']
+    first_image = df_train_x.loc[1, :]
+    first_label = df_train_y[1]
+    plt.imshow(np.reshape(first_image.values, (28, 28)), cmap='gray_r', interpolation='none')
+    plt.title('Digit Label: {}'.format(first_label))
+    plt.show()
+    #standardize data
+    df_train_x = sc.fit_transform(df_train_x)
+    print('df_train rescaled : ', df_train_x)
+    print('df_train_y : ', df_train_y)
+
+    #df_test_y = to_categorical(df_test_y,10)
+    #df_train_y = to_categorical(df_train_y,10)
+
+    return df_test_x, df_test_y,df_train_x, df_train_y
+
+if __name__=="__main__":
+    df_test_x, df_test_y,df_train_x, df_train_y =load_data_from_csv()
+    print('trainX shape :', df_train_x.shape)
+    print('trainy shape :', df_train_y.shape)
+    print('testX shape :', df_test_x.shape)
+    print('testY shape :', df_test_y.shape)
